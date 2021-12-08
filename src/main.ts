69e5b65bb7aa05dc80d3f9f16f14a2d28569cb37
fuboki10 <abdelrahman.tarek1910@gmail.com>
2021-12-09 00:03:28 +0200

    add OpenApi support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
+import { Logger, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  const globalPrefix = 'api';
+
+  /** Set prefices for app url */
+  app.setGlobalPrefix(globalPrefix);
+
+  /** Add Versioning for the app */
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  /** Add OpenApi support  */
+  const options = new DocumentBuilder()
+    .setTitle('Florence Documentation')
+    .setDescription('The sample API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  /** Run the App */
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
+
 bootstrap();
